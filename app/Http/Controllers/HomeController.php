51741f36b6f518acc913bc5b2bdbc0807ddc6453
Jamie Evans <jamieevans@itg.co.uk>
2020-01-02 12:49:38 +0000

    Added responses and passing to views - added search and gelocated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use DmitryIvanov\DarkSkyApi\DarkSkyApi;
+use Geocoder;
 class HomeController extends Controller
 {
     /**
@@ -24,10 +25,43 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $forecast = (new DarkSkyApi(env('DARK_SKY_KEY')))
-                    ->location(Auth::user()->lat, Auth::user()->lng)
-                    ->forecast();
+        $user = Auth::user();
+        $lat = $user->lat;
+        $lng = $user->lng;
+        $location = $user->preferred_location;
 
-        return view('home');
+        $forecast = (new DarkSkyApi(env('DARK_SKY_KEY')))
+                    ->location($lat, $lng)
+                    ->forecast(['currently', 'daily']);
+        
+        $current = $forecast->currently()->toArray();
+        $daily = $forecast->daily()->toArray();
+
+        // dd($daily);
+        // dd($today);
+
+        return view('page.home', compact('current', 'daily', 'location'));
+    }
+
+    /**
+     * search for weather
+     *
+     * @param  mixed $request
+     *
+     * @return void
+     */
+    public function search(Request $request)
+    {
+        $location = $request->input('search');
+        $coords = Geocoder::getCoordinatesForAddress($location);
+
+        $forecast = (new DarkSkyApi(env('DARK_SKY_KEY')))
+                    ->location($coords['lat'], $coords['lng'])
+                    ->forecast(['currently', 'daily']);
+        
+        $current = $forecast->currently()->toArray();
+        $daily = $forecast->daily()->toArray();
+
+        return view('page.home', compact('current', 'daily', 'location'));
     }
 }
